ad463bbe752a2468b7b25e63d7ca79bc7de34dd5
오승현 <timber3@naver.com>
2024-11-20 21:51:36 +0900

    🎉 [INIT]: 백 프로젝트 Init

---
+++ muzipback/src/main/java/com/sh/muzip/MuzipApplication.java
@@ -0,0 +1,13 @@
+package com.sh.muzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzipApplication.class, args);
+	}
+
+}

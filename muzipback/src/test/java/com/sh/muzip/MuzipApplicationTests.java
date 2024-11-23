1500cbec9d55768042dd283d6db999bec426b5f6
오승현 <timber3@naver.com>
2024-11-23 17:10:10 +0900

    🧱 [INFRA]: .env 및 application.yml 수정

--- muzipback/src/test/java/com/sh/muzip/MuzipApplicationTests.java
+++ muzipback/src/test/java/com/sh/muzip/MuzipApplicationTests.java
@@ -3,11 +3,11 @@ package com.sh.muzip;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MuzipApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class MuzipApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
